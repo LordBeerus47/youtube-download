0f0fc321a2c3e550d0c8f57cf106a1c74b5a7314
Charly Moerdyk <charly.moerdyk@ugent.be>
2024-07-04 20:13:02 +0200

    test debug

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def download_again(title):
 if __name__ == '__main__':
     if not os.path.exists('downloads'):
         os.makedirs('downloads')
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
